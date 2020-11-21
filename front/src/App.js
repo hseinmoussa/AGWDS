735cfee7a9f0c9f53ab6c35e03382b36cc804ec9
Hussein Moussa <hseinmoussa98@gmail.com>
2020-11-21 21:09:52 +0200

    connection footer to database + organization

--- front/src/App.js
+++ front/src/App.js
@@ -21,49 +21,7 @@ class App extends React.Component {
         <Navbar />
         <Aboutus />
         <AS1 />
-
-        <hr></hr>
-        <footer className="App-footer">
-          <div className="footer-section1">
-            <div className="footer-buttons">
-              <SocialIcon
-                url="http://gmail.com"
-                style={{ marginRight: "5px" }}
-                className="click"
-              />
-              <SocialIcon
-                url="http://linkedin.com/in"
-                style={{ marginRight: "5px" }}
-                className="click"
-              />
-              <SocialIcon
-                url="http://twitter.com"
-                style={{ marginRight: "5px" }}
-                className="click"
-              />
-              <SocialIcon
-                url="https://www.github.com"
-                bgColor="#000"
-                style={{ marginRight: "5px" }}
-                className="click"
-              ></SocialIcon>
-              <SocialIcon
-                url="https://www.facebook.com"
-                style={{ marginRight: "5px" }}
-                className="click"
-              />
-              <div className="footer-section2">
-                <p style={{ color: "#fff" }}>
-                  <i className="footer-text">
-                    Copyright {"\u00A9"} <b>2020</b>
-                  </i>
-                </p>
-              </div>
-            </div>
-
-            {/* <div className="footer-section">Home | AboutMe</div> */}
-          </div>
-        </footer>
+        <Footer />
       </div>
     );
   }
