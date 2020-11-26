ced7c80321f893862d8f77c0ea80556dd4dc3662
Hussein Moussa <hseinmoussa98@gmail.com>
2020-11-26 23:33:28 +0200

    if logged in login => dashboard

--- front/src/App.js
+++ front/src/App.js
@@ -7,10 +7,18 @@ import Cardsbox from "./components/Cardsbox/Cardsbox";
 import LoginPage from "./components/LoginPage/login";
 import Footer from "./components/Footer/Footer.js";
 import Routes from "./components/dashboard/routes";
-import Protection from './components/LoginPage/Protection';
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Protection from "./components/LoginPage/Protection";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect,
+} from "react-router-dom";
+import Cookies from "universal-cookie";
 
 const Dashboard = () => <Routes />;
+const cookies = new Cookies();
 
 class App extends React.Component {
   render() {
@@ -20,27 +28,37 @@ class App extends React.Component {
         <Link to="/about">about </Link>
         <Link to="/dashboard">dashboard </Link>
         <a href="/login">login </a>
-        
-          <div className="App">
-            <Navbar />
-            <Switch>
-              <Route exact path="/">
-                <Cardsbox />
-              </Route>
-              <Route path="/about">
-                <Aboutus />
-                <AS1 />
-              </Route>
-              <Protection path="/dashboard" >
-                <Dashboard />
-              </Protection>
-              <Route path="/login">
-                <LoginPage />
-                <Cardsbox />
-              </Route>
-            </Switch>
-          </div>
-       
+
+        <div className="App">
+          <Navbar />
+          <Switch>
+            <Route exact path="/">
+              <Cardsbox />
+            </Route>
+            <Route path="/about">
+              <Aboutus />
+              <AS1 />
+            </Route>
+            <Protection path="/dashboard">
+              <Dashboard />
+            </Protection>
+            <Route path="/login">
+              {!cookies.get("token") ? (
+                <>
+                  <LoginPage />
+                  <Cardsbox />
+                </>
+              ) : (
+                <Redirect
+                  to={{
+                    pathname: "/dashboard",
+                  }}
+                />
+              )}
+            </Route>
+          </Switch>
+        </div>
+
         <Footer />
       </Router>
     );
