00fa8a76b0198448a0832b4661934ce00f0d4655
khalil zaarour <k.zaarour1997@gmail.com>
2020-12-02 23:14:31 +0200

    update

--- front/src/components/Navbar/nav.js
+++ front/src/components/Navbar/nav.js
@@ -1,30 +1,24 @@
 import React, { Component } from "react";
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 import "./nav.css";
 import { MenuItems } from "./MenuItems";
 import { Button } from "./button";
-import { Combobox } from "downshift";
-import { Input } from "antd";
+import { BM } from "./button-mobile";
 
 class Navbar extends Component {
   state = { clicked: false, search: "" };
 
-
-
   handleClick = () => {
     this.setState({ clicked: !this.state.clicked });
- 
-  }
+  };
 
   handleInput = (e) => {
-      this.setState({...this.state, search: e.target.value })
-  
-  }
-
+    this.setState({ ...this.state, search: e.target.value });
+  };
 
   handleSubmit = (props) => {
-    this.props.handleSearch({search : this.state.search })
-  }
+    this.props.handleSearch({ search: this.state.search });
+  };
 
   render() {
     return (
@@ -35,7 +29,7 @@ class Navbar extends Component {
             className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}
           ></i>
         </div>
-        <ul className={this.state.clicked ? "nav-menu active" : "nav-menu"}>
+        <div className={this.state.clicked ? "nav-menu active" : "nav-menu"}>
           {MenuItems.map((item, index) => {
             return (
               <li key={index}>
@@ -45,8 +39,34 @@ class Navbar extends Component {
               </li>
             );
           })}
-        </ul>
-        <form className="SF" onSubmit={(e) => {e.preventDefault(); this.handleSubmit() }} >
+          <form
+            class="SF-mobile"
+            onSubmit={(e) => {
+              e.preventDefault();
+              this.handleSubmit();
+            }}
+          >
+            <BM>
+              <i className="fas fa-search"></i>
+            </BM>
+
+            <input
+              className="SB-mobile"
+              onChange={this.handleInput}
+              label="Search Gallery"
+              type="text"
+              placeholder="Search Art..."
+              required
+            />
+          </form>
+        </div>
+        <form
+          className="SF"
+          onSubmit={(e) => {
+            e.preventDefault();
+            this.handleSubmit();
+          }}
+        >
           <Button>
             <i className="fas fa-search"></i>
           </Button>
